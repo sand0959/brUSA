de3028b2b6975eb6041c77f2813d2a3fe893f24e
ted sandridge <sand0959@gmail.com>
2017-04-08 11:16:15 -0400

    java saturday

--- logic.js
+++ logic.js
@@ -1,6 +1,11 @@
 var mapKey = "AIzaSyANiTNnqGnd6RBhUylmCbKiyNONnEipbS8";
 var newLocationValue = '';
 var cityStateLocation = '';
+var longitude = '';
+var latitude = '';
+var coords = {};
+var cityName = '';
+var cityNameWithPlus = '';
 
 
 function generateMap() {};
@@ -8,8 +13,8 @@ function generateMap() {};
 $('body').on('click', '#addLocation', function(event) {
     event.preventDefault();
 
-    cityStateLocation = $("#searchResults").val();
-    console.log(cityStateLocation);
+    cityStateLocation = $("#searchBar").val();
+    // console.log(cityStateLocation);
     csStringwithPlus = cityStateLocation.replace(/ /g, '+');
     console.log(csStringwithPlus);
     queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + csStringwithPlus + "&sensor=false";
@@ -18,10 +23,48 @@ $('body').on('click', '#addLocation', function(event) {
             url: queryURL,
             method: 'GET'
         })
-        .done(function(results) {
-            // for (var i = 0; i < results.location; i++) {
-            //         console.log(results.location[i]);
-            //       }
-            console.log(results.geometry);
+        .done(function(longlat) {
+
+            latitude = parseFloat(longlat.results[0].geometry.location.lat);
+            longitude = parseFloat(longlat.results[0].geometry.location.lng);
+            cityName = longlat.results[0].address_components[0].short_name;
+            coords = { lat: latitude, lng: longitude };
+            cityNameWithPlus = cityName.replace(/ /g, '+');
+            initMap();
+            // initMarker();
+            console.log("http://api.brewerydb.com/v2/locations?key=464c2923de039584755184680e90203c&locality=" + cityNameWithPlus);
+            console.log("http://api.brewerydb.com/v2/locations?key=464c2923de039584755184680e90203c&locality=Durham");
+        });
+
+    function initMap() {
+        map = new google.maps.Map(document.querySelector('#map'), {
+            zoom: 15,
+            center: coords
+        });
+    }
+})
+$('body').on('click', '#addLocation', function(event) {
+    
+    event.preventDefault();
+    
+    breweryURL = "http://api.brewerydb.com/v2/locations?key=464c2923de039584755184680e90203c&locality=" + cityNameWithPlus;
+    $.ajax({
+            url: breweryURL,
+            method: 'GET',
+            crossDomain: 'true',
+            headers: {
+                Accept: "application / json",
+                'Access - Control - Allow - Origin': ' * '
+            }
         })
+        .done(function(brewery) {
+
+            console.log(brewery.data.longtitude);
+            console.log(csStringwithPlus);
+
+        })
+        // for (var i = 0; i < 10; i++) {
+        //     [i]
+        // }
+        //     })
 });
