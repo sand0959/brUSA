8bf7c6677cbf9339fed0840d351e45347871d35c
Noah King <noahking@Noahs-MacBook-Pro.local>
2017-04-10 19:43:49 -0400

    new code

--- logic.js
+++ logic.js
@@ -1,64 +1,74 @@
-
-
 var mapKey = "AIzaSyANiTNnqGnd6RBhUylmCbKiyNONnEipbS8";
 var newLocationValue = '';
 var cityStateLocation = '';
-var csStringwithPlus = '';
-var breweryCity = '';
+var longitude = '';
+var latitude = '';
+var coords = {};
+var cityName = '';
+var cityNameWithPlus = '';
+
+var breweryKey = "464c2923de039584755184680e90203c";
 
 
 
 function generateMap() {};
 
 $('body').on('click', '#addLocation', function(event) {
-    event.preventDefault();
+   event.preventDefault();
 
-    cityStateLocation = $("#searchBar").val();
-    console.log(cityStateLocation);
-    csStringwithPlus = cityStateLocation.replace(/ /g, '+');
-    console.log(csStringwithPlus);
-    queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + csStringwithPlus + "&sensor=false";
-    console.log(queryURL);
-    $.ajax({
-            url: queryURL,
-            method: 'GET'
-        })
-        .done(function(LongLat) {
-            // for (var i = 0; i < results.location; i++) {
-            //         console.log(results.location[i]);
-            //       }
-            console.log(LongLat.results[0].geometry.location.lat);
-        })
-});
+    $('html, body').animate({
+    scrollTop: $("#map").offset().top
+}, 1000);
+
+   cityStateLocation = $("#searchBar").val();
+
+   csStringwithPlus = cityStateLocation.replace(/ /g, '+');
+   console.log(csStringwithPlus);
+   queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + csStringwithPlus + "&sensor=false";
+   console.log(queryURL);
+   $.ajax({
+           url: queryURL,
+           method: 'GET'
+       })
+
+           .done(function(longlat) {
+
+           latitude = parseFloat(longlat.results[0].geometry.location.lat);
+           longitude = parseFloat(longlat.results[0].geometry.location.lng);
+           cityName = longlat.results[0].address_components[0].short_name;
+           coords = { lat: latitude, lng: longitude };
+           cityNameWithPlus = cityName.replace(/ /g, '+');
+           var breweryURL = "http://api.brewerydb.com/v2/locations?key=464c2923de039584755184680e90203c&locality=" + cityNameWithPlus;
+           initMap();
+           $.ajax({
+       url: breweryURL,
+       method: 'GET',
+       crossDomain: 'true',
+      headers: {
+           Accept : "application/json",
+                  'Access-Control-Allow-Origin': '*'
+              }
+   })
+           .done(function(brewery){
+               // for (var i = 0; i < 10; i++) {
+               //     brewery.data.longitude[i],
+               //     brewery.data.latitude[i]
+               //     console.log(brewery.data.longitude)
+               console.log(brewery.data.latitude);
+               console.log(brewery.data[0].latitude);
+               });
 
 
 
-var breweryKey = "464c2923de039584755184680e90203c";
-var breweryURL = "http://api.brewerydb.com/v2/locations?key=464c2923de039584755184680e90203c&locality=durham";
+            })
+           // initMarker();
+           //console.log(breweryURL);
+           
+       });
 
-$('body').on('click', '#addLocation', function(event){
-    event.preventDefault();
-
-    cityLocation = $("#searchBar").val();
-    city = cityLocation;
-    console.log(cityLocation);
-    console.log(city);
-
-    $.ajax({
-        url: breweryURL,
-        method: 'GET',
-        crossDomain: 'true',
-       headers: {
-            Accept : "application/json",
-                   'Access-Control-Allow-Origin': '*'
-               }
-    })
-    .done(function(brewery) {
-
-        console.log(brewery.data.longtitude);
-        console.log(csStringwithPlus);
-
-    })
-
-});
-
+function initMap() {
+       map = new google.maps.Map(document.querySelector('#map'), {
+           zoom: 15,
+           center: coords
+       });
+   }
